d68d7d7b93e4f6bc734f1b3f045215aed96c7e12
Audy <amaudy@gmail.com>
2022-02-23 18:55:43 +0700

    Added Terraform output

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "server_ip" {
+    value = aws_instance.awsdevbox.public_ip
+    description = "IP to server"
+}
+
+output "private_key" {
+  value       = tls_private_key.pk.private_key_pem
+  description = "Private key to access the EC2 instances"
+  sensitive   = true
+}
